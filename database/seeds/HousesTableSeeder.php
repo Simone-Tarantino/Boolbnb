2be6a0d7f20e642ec82aba767b5b2aee91c8c8e5
Simone Tarantino <tarantinosimonew@gmail.com>
2020-04-15 15:44:40 +0200

    Seeder house

---
+++ database/seeds/HousesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\House;
+use App\User;
+use Faker\Generator as Faker;
+
+class HousesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 3; $i++) {
+            $newHouse = new House;
+            $newHouse->user_id = User::inRandomOrder()->first()->id;
+            $newHouse->room_number = $faker->numberBetween($min = 1, $max = 5);
+            $newHouse->bed = $faker->numberBetween($min = 1, $max = 10);
+            $newHouse->bathroom = $faker->numberBetween($min = 1, $max = 5);
+            $newHouse->mq = $faker->randomFloat($nbMaxDecimals = 2, $min = 50, $max = 900);
+            $newHouse->address = $faker->address();
+            $newHouse->img_path = 'https://picsum.photos/200';
+            $newHouse->status = rand(0, 1);
+            $newHouse->save();                             
+        }
+    }
+
+}

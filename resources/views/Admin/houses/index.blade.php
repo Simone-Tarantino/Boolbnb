dcac45463f540ac8d8228b901f633b8bb36669df
Matteo Magliano <matteo.magliano01@gmail.com>
2020-04-16 15:57:12 +0200

    crud completa def

--- resources/views/Admin/houses/index.blade.php
+++ resources/views/Admin/houses/index.blade.php
@@ -5,16 +5,21 @@
 @if (Auth::id()==$house->user_id)
     <h2>Numero appartamento {{$house->id}}, pubblicato da {{$house->user_id}}</h2>
         <ul>
-            <li>{{$house->room_number}}</li>
-            <li>{{$house->bed}}</li>
-            <li>{{$house->bathroom}}</li>
-            <li>{{$house->mq}}</li>
-            <li>{{$house->address}}</li>
-            <li>{{$house->img_path}}</li>
-            <li>{{$house->created_at}}</li>
-            <li>{{$house->updated_at}}</li>
-            <li>{{$house->status}}</li>
+            <li>Numero Stanze:{{$house->room_number}}</li>
+            <li>Numero Letti: {{$house->bed}}</li>
+            <li>Numero Bagni:{{$house->bathroom}}</li>
+            <li>Metri Quadri Appartamento: {{$house->mq}}</li>
+            <li>Indirizzo: {{$house->address}}</li>
+            <li>Foto: {{$house->img_path}}</li>
+            <li>Caricato il:{{$house->created_at}}</li>
+            <li>Modificato il: {{$house->updated_at}}</li>
+            @if ($house->status == 1)
+            <li>Pubblicato: Si</li> 
+            @else 
+               <li>Pubblicato: no</li>
+            @endif
             <li><a href="{{route('admin.houses.show', $house)}}">Mostra appartamento</a></li>
+            <li><h3>Servizi extra</h3></li>
             @foreach ($house->extras as $extra)
             <li>{{$extra->name}}</li>
             
--- resources/views/Admin/houses/show.blade.php
+++ resources/views/Admin/houses/show.blade.php
@@ -3,18 +3,23 @@
 @section('main')
     <h2>Numero appartamento {{$house->id}}, pubblicato da {{$house->user_id}}</h2>
         <ul>
-            <li>{{$house->room_number}}</li>
-            <li>{{$house->bed}}</li>
-            <li>{{$house->bathroom}}</li>
-            <li>{{$house->mq}}</li>
-            <li>{{$house->address}}</li>
-            <li>{{$house->img_path}}</li>
-            <li>{{$house->created_at}}</li>
-            <li>{{$house->updated_at}}</li>
-            <li>{{$house->status}}</li>
+            <li>Numero stanze:{{$house->room_number}}</li>
+            <li>Numero letti:{{$house->bed}}</li>
+            <li>Numero bagni:{{$house->bathroom}}</li>
+            <li>Metri Quadri Appartamento: {{$house->mq}}</li>
+            <li>Indirizzo: {{$house->address}}</li>
+            <li>Foto: {{$house->img_path}}</li>
+            <li>Caricato il: {{$house->created_at}}</li>
+            <li>Modificato il: {{$house->updated_at}}</li>
+            @if ($house->status == 1)
+            <li>Pubblicato: Si</li> 
+            @else 
+               <li>Pubblicato: no</li>
+            @endif
             <li><a href="{{route('admin.houses.edit', $house)}}">Modifica dati</a></li>
         </ul>
         <ul>
+        <li><h3>Servizi extra</h3></li>
         @foreach ($house->extras as $extra)
         <li>{{$extra->name}}</li>
             

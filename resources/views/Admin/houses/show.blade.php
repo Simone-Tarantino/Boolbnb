330f768d5cae82e2ef156b565d259fd2ef8d7fe2
Guy Roger Eboulet <eboulet87@gmail.com>
2020-04-17 10:48:19 +0200

    Latitudine e longitudine

--- resources/views/Admin/houses/show.blade.php
+++ resources/views/Admin/houses/show.blade.php
@@ -27,4 +27,57 @@
             
         @endforeach
         </ul>
+          <div id='map'>
+
+        </div>
+        <script>
+
+            // Definisco una variabile con le cordinate di longitudine e latitudine dell'appartamento
+            var cordinateAppartamento = [-121.91595, 37.36729];
+            var map = tt.map({
+                container: 'map',
+                key: 'jmSHc4P5sMLTeiGeWWoRL81YcCxYxqGp',
+                style: 'tomtom://vector/1/basic-main',
+                center: cordinateAppartamento,
+                zoom: 15
+            });
+            //Aggiungo un punto d'interesse all'interno della mappa
+            var marker = new tt.Marker().setLngLat(cordinateAppartamento).addTo(map);
+
+            //Aggiungo un pop up all'interno della mappa
+            var popupOffsets = {
+              top: [0, 0],
+              bottom: [0, -70],
+              'bottom-right': [0, -70],
+              'bottom-left': [0, -70],
+              left: [25, -35],
+              right: [-25, -35]
+            }
+            //Aggiungo le informazioni del nostro appartamento
+            var popup = new tt.Popup({offset: popupOffsets}).setHTML("<b>Il nostro Appartamento</b><br/>100 Century Center Ct 210, San Jose, CA 95112, USA");
+            marker.setPopup(popup).togglePopup();
+
+
+						$(document).ready(function() {
+            //Chiamata ajax
+							$.ajax(
+								 {
+								 url: "https://api.tomtom.com/search/2/structuredGeocode.json?countryCode=IT&municipality=Milano&key=jmSHc4P5sMLTeiGeWWoRL81YcCxYxqGp",
+								 method: "GET",
+								 success: function (data, stato) {
+									 // console.log(data);
+								 // $("#risultati").html(data);
+								 processData(data.results);
+								 },
+								 error: function (richiesta, stato, errori) {
+								 alert("E' avvenuto un errore. " + errore);
+								 }
+								 }
+								);
+						});
+
+						function processData (data) {
+							console.log(data);
+						}
+        </script>
 @endsection
--- resources/views/layouts/partials/_head.blade.php
+++ resources/views/layouts/partials/_head.blade.php
@@ -4,5 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' type='text/css' href='https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.37.2/maps/maps.css'/>
+        <script src='https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.37.2/maps/maps-web.min.js'></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js" charset="utf-8"></script>
+        <style>
+            #map {
+              height: 500px;
+              width: 500px;
+          }
+        </style>
     <title>Document</title>
 </head>
